03ce920085ed21c37a4773e494ac99aea96d2e8a
Ankit Verma <all.hail.ankit@gmail.com>
2019-11-17 20:25:01 -0500

    messy ass multi-platform playlists for tmrs demo

--- components/PlaylistSongsScreen.js
+++ components/PlaylistSongsScreen.js
@@ -1,21 +1,94 @@
 import React, {Component} from 'react';
 import { Icon, SearchBar, ListItem, Avatar } from 'react-native-elements'
 import { FlatList, ScrollView, StyleSheet, View, Text, Image } from 'react-native';
+import WebView from 'react-native-webview'
 
 export default class PlaylistSongsScreen extends Component {
     render() {
         return (
-            <FlatList
-                data={DATA}
-                renderItem={({ item }) => (
-                    <Song
-                      title={item.title}
-                      artist={item.artist}
-                      length={item.length}
-                      albumArt={item.albumArt}
-                    />
-                )}
+            <>
+            <WebView 
+                style={styles.item}
+                ref={webview => {
+                    this.webView = webview;
+                }}
+                onLoadEnd={this.onWebViewLoad}
+                onMessage={this.messageEvent}
+                startInLoadingState={true}
+                javaScriptEnabled={true}
+                source={{html: '<iframe src="https://www.youtube.com/embed/Nauusq5yCi0" allow="autoplay; encrypted-media"></iframe>'}}
             />
+            <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                }}
+            />
+            <WebView 
+                style={styles.item}
+                ref={webview => {
+                    this.webView = webview;
+                }}
+                onLoadEnd={this.onWebViewLoad}
+                onMessage={this.messageEvent}
+                startInLoadingState={true}
+                javaScriptEnabled={true}
+                source={{html: '<iframe src="https://open.spotify.com/embed/track/35bNJROxx1h4Y72yG3xShY" allow="autoplay; encrypted-media"></iframe>'}}
+            />
+            <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                }}
+            />
+            <WebView 
+                style={styles.item}
+                ref={webview => {
+                    this.webView = webview;
+                }}
+                onLoadEnd={this.onWebViewLoad}
+                onMessage={this.messageEvent}
+                startInLoadingState={true}
+                javaScriptEnabled={true}
+                source={{html: '<iframe src="https://www.youtube.com/embed/cqyziA30whE" allow="autoplay; encrypted-media"></iframe>'}}
+            />
+            </>
+            // <FlatList
+            //     data={[
+            //         {key: '<iframe src="https://www.youtube.com/embed/cqyziA30whE" allow="autoplay; encrypted-media"></iframe>'},
+            //         {key: '<iframe src="https://www.youtube.com/embed/Nauusq5yCi0" allow="autoplay; encrypted-media"></iframe>'},
+            //         {key: '<iframe src="https://www.youtube.com/embed/cqyziA30whE" allow="autoplay; encrypted-media"></iframe>'},
+            //     ]}
+            //     renderItem={({item}) => 
+            //     <WebView 
+            //         style={{width: 1000, marginHorizontal: 40}}
+            //         ref={webview => {
+            //             this.webView = webview;
+            //         }}
+            //         onLoadEnd={this.onWebViewLoad}
+            //         onMessage={this.messageEvent}
+            //         startInLoadingState={true}
+            //         javaScriptEnabled={true}
+            //         source={{html: item.key}}
+            //     />}
+            // />
+
+
+
+
+
+            
+            // <FlatList
+            //     data={DATA}
+            //     renderItem={({ item }) => (
+            //         <Song
+            //           title={item.title}
+            //           artist={item.artist}
+            //           length={item.length}
+            //           albumArt={item.albumArt}
+            //         />
+            //     )}
+            // />
         );
     }
 }
@@ -54,3 +127,25 @@ const DATA = [
         albumArt: require('../assets/empty_album_art.png'),
     },
 ];
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    width: 1000, height: 115, marginHorizontal: 40, marginVertical: 20 
+  },
+})
+
+// <WebView
+//                 style={{width: 1000, marginHorizontal: 40}}
+//                 ref={webview => {
+//                     this.webView = webview;
+//                 }}
+//                 onLoadEnd={this.onWebViewLoad}
+//                 onMessage={this.messageEvent}
+//                 startInLoadingState={true}
+//                 javaScriptEnabled={true}
+//                 source={{html: `<iframe src="https://www.youtube.com/embed/cqyziA30whE" allow="autoplay; encrypted-media"></iframe>`}}
+//             />
